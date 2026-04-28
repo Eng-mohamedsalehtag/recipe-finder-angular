f39092ddfc1d64c06cc5edd86bf42064b990b99c
Mohamed Saleh <mosalehtag74@icloud.com>
2026-04-29 02:23:34 +0300

    edite cart component

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -8,11 +8,19 @@ export class CartService {
   private cartItems = new BehaviorSubject<any[]>([]);
   cart$ = this.cartItems.asObservable();
   addToCart(product: any) {
-    const currentItems = this.cartItems.value;
-    const existingItem = currentItems.find((item) => item.id === product.id);
-    if (existingItem) return;
+    const current = this.cartItems.value;
 
-    this.cartItems.next([...currentItems, product]);
+    const existing = current.find((item) => item.id === product.id);
+
+    if (existing) {
+      // 🔥 زوّد الكمية
+      existing.quantity += 1;
+    } else {
+      // 🔥 أول مرة
+      current.push({ ...product, quantity: 1 });
+    }
+
+    this.cartItems.next([...current]);
   }
 
   removeFromCart(Id: string) {
